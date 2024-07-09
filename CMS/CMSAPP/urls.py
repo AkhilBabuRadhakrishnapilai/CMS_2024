ef3c011575cb75bf4c1d5d8902cde4c407dd4e89
Emma <emma@gmail.com>
2024-07-09 17:39:38 +0530

    Admin+stock integrated api

--- CMS/CMSAPP/urls.py
+++ CMS/CMSAPP/urls.py
@@ -18,5 +18,19 @@ urlpatterns = [
     #role
     path('roles/',GetAllRoles.as_view()),
     #change password
-    path('changepassword/',ChangePassword.as_view())
+    path('changepassword/',ChangePassword.as_view()),
+
+     #stock_management urls
+    path('suppliers', supplier_list, name='supplier-list'),
+    path('suppliers/<int:passed_id>', supplier_edit, name='supplier-edit'),
+    path('orders', order_list, name='order_list'),
+    path('orders/<int:passed_id>', order_edit, name='orders-edit'),
+    path('equipments', equipment_list, name='equipment_list'),
+    path('equipments/<int:passed_id>', equipment_edit, name='equipment_edit'),
+    path('MiscellaneousItem', MiscellaneousItem_list, name='MiscellaneousItem_list'),
+    path('MiscellaneousItem/<int:passed_id>', MiscellaneousItem_edit, name='MiscellaneousItem_edit'),
+    path('Medicine', Medicine_list, name='Medicine_list'),
+    path('Medicine/<int:passed_id>', Medicine_edit, name='Medicine_edit'),
+
+
 ]
