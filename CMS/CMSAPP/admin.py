30b0cb6d81274ed0847acda65156bcc801bcc657
abr <akhilbn4@gmail.com>
2024-07-11 10:44:13 +0530

    some changes

--- CMS/CMSAPP/admin.py
+++ CMS/CMSAPP/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import *
-from .models import patient_details
 # Register your models here.
 admin.site.register(patient_details)
 admin.site.register(BookAppointment)
